cdcabd854a66df4de5d95caf0853a55686e7fbb1
Filip Bengs <filip.bengs@outlook.com>
2021-09-16 18:18:20 +0300

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # FullStackOpen21
 A collection of completed exercises from the course Full Stack Open 2021 (https://fullstackopen.com/en/)
+
+[part0](https://github.com/filben235/FullStackOpen21/tree/main/part0)
+
+[part1](https://github.com/filben235/FullStackOpen21/tree/main/part1)
+
+[part2](https://github.com/filben235/FullStackOpen21/tree/main/part2)
+
+[part3](https://github.com/filben235/FullStackOpen21_part3)
---
+++ notesAndExamples/part3/index.js
@@ -0,0 +1,89 @@
+const express = require('express')
+const app = express()
+
+//takes the JSON data of a request, transforms it into a JavaScript object and then attaches it to the body property of the request object
+app.use(express.json())
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true
+  }
+]
+
+
+app.get('/', (request, response) => {
+    response.send('<p>moro</p>')
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    //id needs to be parsed since it is a string
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+  
+    if (!body.content) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+  
+    const note = {
+      content: body.content,
+      important: body.important || false,
+      date: new Date(),
+      id: generateId(),
+    }
+  
+    notes = notes.concat(note)
+  
+    response.json(note)
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    //id needs to be parsed since it is a string
+    const id = Number(request.params.id)
+    //removes note that matches id param from array
+    notes = notes.filter(note => note.id !== id)
+    
+    response.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => n.id))
+      : 0
+    return maxId + 1
+}
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
